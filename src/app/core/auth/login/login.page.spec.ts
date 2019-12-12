1f837a233ddf33c5435ce2a8d4f1317ace9fc7ed
ennaque <volkov.enn@gmail.com>
2019-12-12 12:14:51 +0300

    login page test

--- src/app/core/auth/login/login.page.spec.ts
+++ src/app/core/auth/login/login.page.spec.ts
@@ -2,6 +2,12 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { LoginPage } from './login.page';
+import {LoginFormComponent} from '../components/login-form/login-form.component';
+import {ErrorFlashbagComponent} from '../../../shared/components/error-flashbag/error-flashbag.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterTestingModule} from '@angular/router/testing';
+import {TokenManagerService} from '../services/token-manager.service';
+import {TokenManagerServiceMock} from '../components/login-form/login-form.component.spec';
 
 describe('LoginPage', () => {
   let component: LoginPage;
@@ -9,8 +15,11 @@ describe('LoginPage', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginPage ],
-      imports: [IonicModule.forRoot()]
+      declarations: [ LoginPage, LoginFormComponent, ErrorFlashbagComponent ],
+      imports: [IonicModule.forRoot(), ReactiveFormsModule, FormsModule, RouterTestingModule],
+      providers: [
+        { provide: TokenManagerService, useClass: TokenManagerServiceMock },
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginPage);
@@ -20,5 +29,9 @@ describe('LoginPage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should create', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('app-login-form')).not.toBe(null);
   });
 });
