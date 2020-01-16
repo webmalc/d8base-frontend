1a27dbd19baaff9305221f32cb94d83412cbe08f
ennaque <volkov.enn@gmail.com>
2020-01-17 00:40:40 +0300

    save color mode into storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {DarkModeService} from './shared/services/dark-mode.service';
 
 @Component({
   selector: 'app-root',
@@ -16,9 +17,11 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private darkModeService: DarkModeService
   ) {
     this.initializeApp();
+    this.initDarkMode();
   }
 
   initializeApp() {
@@ -26,5 +29,16 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  private initDarkMode() {
+    this.darkModeService.isDarkMode().then((data: boolean) => {
+      this.darkTheme = data;
+    });
+  }
+
+  public changeMode(data: any) {
+    this.darkTheme = data.target.checked;
+    this.darkModeService.setMode(data.target.checked);
   }
 }
