7e37b6e5fc0f06336ff9d1e63d4e4164bf058a80
ennaque <volkov.enn@gmail.com>
2020-02-19 17:00:18 +0300

    add headersInterceptor test

--- src/app/core/services/headers-interceptor.service.spec.ts
+++ src/app/core/services/headers-interceptor.service.spec.ts
@@ -1,12 +1,61 @@
-// import { TestBed } from '@angular/core/testing';
-//
-// import { ApiInterceptorService } from './api-interceptor.service';
-//
-// describe('ApiInterceptorService', () => {
-//   beforeEach(() => TestBed.configureTestingModule({}));
-//
-//   it('should be created', () => {
-//     const service: ApiInterceptorService = TestBed.get(ApiInterceptorService);
-//     expect(service).toBeTruthy();
-//   });
-// });
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {fakeAsync, TestBed, tick} from '@angular/core/testing';
+import {environment} from '../../../environments/environment';
+import {ApiClientService} from './api-client.service';
+import {HeadersInterceptor} from './headers-interceptor.service';
+import {TokenManagerService} from './token-manager.service';
+
+
+describe('HeadersInterceptor', () => {
+
+    let client: jasmine.SpyObj<ApiClientService>;
+    let tokenManager: jasmine.SpyObj<TokenManagerService>;
+    let service: HeadersInterceptor;
+    let httpMock: HttpTestingController;
+
+    beforeEach(() => {
+        const spyTokenManager = jasmine.createSpyObj(
+            'TokenManagerService', {
+                getAccessToken: () => Promise.resolve('access_token')
+            }
+        );
+        TestBed.configureTestingModule({
+            imports: [
+                HttpClientTestingModule
+            ],
+            providers: [
+                {
+                    provide: HTTP_INTERCEPTORS,
+                    useClass: HeadersInterceptor,
+                    multi: true
+                },
+                {
+                    provide: TokenManagerService,
+                    useValue: spyTokenManager
+                },
+                HeadersInterceptor
+            ]
+        });
+
+        client = TestBed.get(ApiClientService);
+        service = TestBed.get(HeadersInterceptor);
+        tokenManager = TestBed.get(TokenManagerService);
+        httpMock = TestBed.get(HttpTestingController);
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should save new tokens if access token has expired', fakeAsync(() => {
+        tokenManager.getAccessToken.and.returnValue(Promise.resolve('access_token'));
+
+        client.get('/test/').subscribe();
+        tick();
+        const req = httpMock.expectOne(environment.backend.url + '/test/');
+        tick();
+        expect(req.request.headers.has('Authorization')).toEqual(true);
+        expect(req.request.headers.get('Authorization')).toEqual('Bearer access_token');
+    }));
+});
