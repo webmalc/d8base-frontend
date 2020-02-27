5ebb829456f9fbba82560e981a30d597213fc261
ennaque <volkov.enn@gmail.com>
2020-02-27 15:17:33 +0300

    refactor env urls

--- src/app/auth/services/registration.service.ts
+++ src/app/auth/services/registration.service.ts
@@ -13,7 +13,7 @@ import {environment} from '../../../environments/environment';
 @Injectable()
 export class RegistrationService {
 
-    private readonly REGISTER_URL = environment.backend.register;
+    private readonly REGISTER_URL = environment.backend.user;
 
     constructor(
         protected client: ApiClientService,
--- src/app/core/services/user-manager.service.ts
+++ src/app/core/services/user-manager.service.ts
@@ -1,8 +1,8 @@
+import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {UserInterface} from '@app/shared/interfaces/user.interface';
-import {HttpClient} from '@angular/common/http';
-import {environment} from '../../../environments/environment';
 import {Observable} from 'rxjs';
+import {environment} from '../../../environments/environment';
 
 @Injectable({
     providedIn: 'root'
@@ -15,7 +15,7 @@ export class UserManagerService {
     }
 
     public getUser(): Observable<any> {
-        return this.http.get(environment.backend.url + environment.backend.get_user_data_url);
+        return this.http.get(environment.backend.url + environment.backend.user);
     }
 
     public updateUser(user: UserInterface): void {
