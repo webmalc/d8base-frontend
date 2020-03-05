1470591bb258baf65da88d22a630ae357f9f457d
ennaque <volkov.enn@gmail.com>
2020-03-05 20:44:30 +0300

    fix translations

--- src/app/core/services/translation.service.ts
+++ src/app/core/services/translation.service.ts
@@ -1,4 +1,5 @@
 import {Injectable} from '@angular/core';
+import {StorageManagerService} from '@app/core/proxies/storage-manager.service';
 import {TranslateService} from '@ngx-translate/core';
 import {Observable} from 'rxjs';
 
@@ -9,23 +10,33 @@ export class TranslationService {
 
     public static readonly DIR = './assets/i18n/';
     public static readonly SUFFIX = '.json';
-
     public readonly LANGUAGES = {
         ru: 'ru',
         en: 'en'
     };
+    private readonly STORAGE_KEY = 'lang';
 
-    constructor(private translator: TranslateService) {
+    constructor(private translator: TranslateService, private storageManager: StorageManagerService) {
     }
 
     public init(): void {
-        this.translator.setDefaultLang(this.LANGUAGES.en);
+        this.getStorage().then(
+            (defaultLang: string | null) => {
+                if (defaultLang !== null) {
+                    this.translator.setDefaultLang(defaultLang);
+                } else {
+                    this.translator.setDefaultLang(this.LANGUAGES.en);
+                    this.setStorage(this.LANGUAGES.en);
+                }
+            }
+        );
 
         return;
     }
 
     public setLang(lang: string): void {
         this.translator.setDefaultLang(lang);
+        this.setStorage(lang);
     }
 
     public trans(key?: string): Observable<string | any> {
@@ -38,5 +49,13 @@ export class TranslationService {
 
     public getLanguagesAsArray(): Array<string> {
         return Object.values(this.LANGUAGES);
+    }
+
+    private setStorage(lang: string): void {
+        this.storageManager.set(this.STORAGE_KEY, lang);
+    }
+
+    private getStorage(): Promise<string> {
+        return this.storageManager.get(this.STORAGE_KEY);
     }
 }
