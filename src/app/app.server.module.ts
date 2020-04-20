9ebf75c3a969ae569d332633e25d1654e686f892
zalex <zalex@zalex.com.ua>
2020-04-20 20:07:08 +0300

    Add IonicServermodule to NgModule directive ins app.server.module.ts

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,14 +1,17 @@
-import { NgModule } from '@angular/core';
-import { ServerModule } from '@angular/platform-server';
+import {NgModule} from '@angular/core';
+import {ServerModule} from '@angular/platform-server';
 
-import { AppModule } from './app.module';
-import { AppComponent } from './app.component';
+import {IonicServerModule} from '@ionic/angular-server';
+import {AppComponent} from './app.component';
+import {AppModule} from './app.module';
 
 @NgModule({
-  imports: [
-    AppModule,
-    ServerModule,
-  ],
-  bootstrap: [AppComponent],
+    imports: [
+        AppModule,
+        ServerModule,
+        IonicServerModule
+    ],
+    bootstrap: [AppComponent],
 })
-export class AppServerModule {}
+export class AppServerModule {
+}
