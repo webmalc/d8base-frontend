900bac2f3cd1c479921c0cabebdc407c346ec929
ennaque <volkov.enn@gmail.com>
2020-05-12 18:04:23 +0300

    fill master contacts form with profile data

--- src/app/master/components/contacts-tab/contacts-tab.component.ts
+++ src/app/master/components/contacts-tab/contacts-tab.component.ts
@@ -1,8 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {ApiListResponseInterface} from '@app/core/interfaces/api-list-response.interface';
 import {MasterContact} from '@app/master/models/master-contact';
 import {MasterContactsApiService} from '@app/master/services/master-contacts-api.service';
+import {UserContact} from '@app/profile/models/user-contact';
+import {UserContactApiService} from '@app/profile/services/user-contact-api.service';
 import {ClientContactInterface} from '@app/shared/interfaces/client-contact-interface';
+import {AlertController} from '@ionic/angular';
 
 @Component({
     selector: 'app-master-contacts-tab',
@@ -12,11 +16,19 @@ import {ClientContactInterface} from '@app/shared/interfaces/client-contact-inte
 export class ContactsTabComponent implements OnInit {
 
     public masterId: number;
+    public masterContactsData: MasterContact[] = [];
 
-    constructor(public masterContactsApiService: MasterContactsApiService, private route: ActivatedRoute) { }
+    constructor(
+        public masterContactsApiService: MasterContactsApiService,
+        private route: ActivatedRoute,
+        private userContactsApiService: UserContactApiService,
+        private alert: AlertController
+    ) {
+    }
 
     public ngOnInit(): void {
         this.masterId = parseInt(this.route.snapshot.paramMap.get('id'), 10);
+        this.generateContactsData();
     }
 
     public getNewMasterContact(): () => ClientContactInterface {
@@ -26,5 +38,34 @@ export class ContactsTabComponent implements OnInit {
 
             return contact;
         };
+    }
+
+    private showFillContactsMessage(dataToFill: UserContact[]): void {
+        this.alert.create({
+            header: 'Confirm!',
+            message: 'Fill professional contacts with user contacts data',
+            buttons: [
+                {
+                    text: 'Cancel',
+                    role: 'cancel',
+                    cssClass: 'secondary'
+                }, {
+                    text: 'OK',
+                    handler: () => this.masterContactsData = dataToFill
+                }
+            ]
+        }).then(alert => alert.present());
+    }
+
+    private generateContactsData(): void {
+        this.masterContactsApiService.getByClientId(this.masterId).subscribe(
+            (masterContacts: ApiListResponseInterface<MasterContact>) => {
+                if (masterContacts.results.length === 0) {
+                    this.userContactsApiService.getCurrentClientContacts().subscribe(
+                        (data: ApiListResponseInterface<UserContact>) => this.showFillContactsMessage(data.results)
+                    );
+                }
+            }
+        );
     }
 }
