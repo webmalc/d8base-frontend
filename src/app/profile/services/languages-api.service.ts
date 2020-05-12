ce45a67af4e81430698b2fd6c2c43515e8aa41b2
zalex <zalex@zalex.com.ua>
2020-05-12 19:09:28 +0300

    Fix wrong language endpoint

--- src/app/profile/services/languages-api.service.ts
+++ src/app/profile/services/languages-api.service.ts
@@ -1,5 +1,4 @@
 import {Injectable} from '@angular/core';
-import {ApiListResponseInterface} from '@app/core/interfaces/api-list-response.interface';
 import {ApiClientService} from '@app/core/services/api-client.service';
 import {Language} from '@app/profile/models/language';
 import {plainToClass} from 'class-transformer';
@@ -18,10 +17,10 @@ export class LanguagesApiService {
     }
 
     public getLanguages$(): Observable<Language[]> {
-        return this.client.get<ApiListResponseInterface<Language>>(this.url).pipe(
+        return this.client.get<Language[]>(this.url).pipe(
             map(
-                ({results}) => {
-                    return plainToClass<Language, object>(Language, results, {excludeExtraneousValues: true});
+                (languages) => {
+                    return plainToClass<Language, object>(Language, languages, {excludeExtraneousValues: true});
                 }
             )
         );
