6940c9fe69eff84c0cf9d0a09e928b651e46e489
enkryptor <enkryptor@yandex.ru>
2020-10-29 13:47:24 +0300

    add firebase to explicitly allowed CommonJS imports

--- src/app/core/services/fcm-device.service.ts
+++ src/app/core/services/fcm-device.service.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@angular/core';
 import {AuthenticationService} from '@app/core/services/authentication.service';
 import {FcmDevicesApiService} from '@app/core/services/fcm-devices-api.service';
-import {firebase} from '@firebase/app';
-import '@firebase/messaging';
+import firebase from 'firebase';
+
 import {filter} from 'rxjs/operators';
 
 @Injectable({
