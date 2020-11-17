700d9a0260f3750270d9861dec04531309f9c37b
enkryptor <enkryptor@yandex.ru>
2020-11-17 19:13:53 +0300

    disable deprecated test

--- src/app/shared/components/picture-selector/image-cropper/image-crop-popover.component.spec.ts
+++ src/app/shared/components/picture-selector/image-cropper/image-crop-popover.component.spec.ts
@@ -1,24 +1,30 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
+import {IonicModule} from '@ionic/angular';
+import {TranslateModule} from '@ngx-translate/core';
 
-import { ImageCropPopoverComponent } from './image-crop-popover.component';
+import {ImageCropPopoverComponent} from './image-crop-popover.component';
 
 describe('ImageCropperComponent', () => {
-  let component: ImageCropPopoverComponent;
-  let fixture: ComponentFixture<ImageCropPopoverComponent>;
+    let component: ImageCropPopoverComponent;
+    let fixture: ComponentFixture<ImageCropPopoverComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ImageCropPopoverComponent ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+    beforeEach(waitForAsync(() => {
+        TestBed.configureTestingModule({
+            declarations: [ImageCropPopoverComponent],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA],
+            imports: [
+                IonicModule.forRoot(),
+                TranslateModule.forRoot()
+            ]
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(ImageCropPopoverComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+        fixture = TestBed.createComponent(ImageCropPopoverComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
