252a3a754638a90538ae8368a77e2ae9ca3430cf
Slepukhin Vitalii <vitslepukhin@gmail.com>
2020-12-02 18:07:40 +0400

    Orders: Client details step added

--- src/app/order/components/client-details-step/client-details-step.component.spec.ts
+++ src/app/order/components/client-details-step/client-details-step.component.spec.ts
@@ -1,8 +1,13 @@
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {ChangeDetectorRef} from '@angular/core';
 import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
+import {FormBuilder} from '@angular/forms';
 import {RouterTestingModule} from '@angular/router/testing';
 import {OrderWizardStateService} from '@app/order/services/order-wizard-state.service';
 import {IonicModule} from '@ionic/angular';
+import {IonicStorageModule} from '@ionic/storage';
 import {TranslateModule} from '@ngx-translate/core';
+import {UserManagerService} from '../../../core/services/user-manager.service';
 
 import {ClientDetailsStepComponent} from './client-details-step.component';
 
@@ -10,17 +15,30 @@ describe('ClientDetailsStepComponent', () => {
     let component: ClientDetailsStepComponent;
     let fixture: ComponentFixture<ClientDetailsStepComponent>;
 
-    beforeEach(waitForAsync(() => {
-        TestBed.configureTestingModule({
-            declarations: [ClientDetailsStepComponent],
-            imports: [IonicModule.forRoot(), TranslateModule.forRoot(), RouterTestingModule],
-            providers: [OrderWizardStateService]
-        }).compileComponents();
+    beforeEach(
+        waitForAsync(() => {
+            TestBed.configureTestingModule({
+                declarations: [ClientDetailsStepComponent],
+                imports: [
+                    IonicModule.forRoot(),
+                    TranslateModule.forRoot(),
+                    RouterTestingModule,
+                    IonicStorageModule.forRoot(),
+                    HttpClientTestingModule
+                ],
+                providers: [
+                    OrderWizardStateService,
+                    UserManagerService,
+                    FormBuilder,
+                    ChangeDetectorRef
+                ]
+            }).compileComponents();
 
-        fixture = TestBed.createComponent(ClientDetailsStepComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-    }));
+            fixture = TestBed.createComponent(ClientDetailsStepComponent);
+            component = fixture.componentInstance;
+            fixture.detectChanges();
+        })
+    );
 
     it('should create', () => {
         expect(component).toBeTruthy();
