e5c49f7d777a2316a57985b1baa73d49e0a3a549
Slepukhin Vitalii <vitslepukhin@gmail.com>
2020-12-04 00:06:59 +0400

    Fix main column size, fix duration negative values

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,6 +2,7 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {IonicModule} from '@ionic/angular';
+import {SharedModule} from '../shared/shared.module';
 import {AuthRoutingModule} from './auth-routing.module';
 
 @NgModule({
@@ -10,7 +11,8 @@ import {AuthRoutingModule} from './auth-routing.module';
         FormsModule,
         IonicModule,
         AuthRoutingModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        SharedModule
     ],
     declarations: []
 })
