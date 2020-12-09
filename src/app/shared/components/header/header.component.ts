943f7ffdca4bb112e314de191a4acf39279ab819
enkryptor <enkryptor@yandex.ru>
2020-12-09 12:48:52 +0300

    removed redundant subscription

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,20 +1,19 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthenticationFactory, MasterManagerService} from '@app/core/services';
 import {UserManagerService} from '@app/core/services/user-manager.service';
 import {Country} from '@app/profile/models/country';
 import {MenuController, Platform} from '@ionic/angular';
-import {Observable, of, Subscription} from 'rxjs';
-import {switchMap} from 'rxjs/operators';
+import {Observable, of} from 'rxjs';
+import {map, share, switchMap} from 'rxjs/operators';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit, OnDestroy {
+export class HeaderComponent implements OnInit {
     public isAuthenticated$: Observable<boolean>;
-    public countryCode: string;
-    private countrySub: Subscription;
+    public countryCode$: Observable<string>;
 
     constructor(
         public readonly masterManager: MasterManagerService,
@@ -26,14 +25,10 @@ export class HeaderComponent implements OnInit, OnDestroy {
         this.isAuthenticated$ = authenticationFactory.getAuthenticator().isAuthenticated$;
     }
 
-    public ngOnDestroy(): void {
-        this.countrySub.unsubscribe();
-    }
-
     public ngOnInit(): void {
-        this.countrySub = this.isAuthenticated$.pipe(switchMap(
+        this.countryCode$ = this.isAuthenticated$.pipe(switchMap(
             isAuth => isAuth ? this.userManager.getDefaultUserCountry() : of(this.getTemporaryDefaultCountry())
-        )).subscribe(c => this.countryCode = c.code.toLowerCase());
+        ), map(c => c.code.toLowerCase()), share());
     }
 
     public isDesktop(): boolean {
