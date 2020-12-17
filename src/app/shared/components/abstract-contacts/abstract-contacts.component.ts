996841d3dd712353acc47b264ba00c885d332d0c
enkryptor <enkryptor@yandex.ru>
2020-12-18 01:34:04 +0300

    refactor: simplify contacts component

--- src/app/shared/components/abstract-contacts/abstract-contacts.component.ts
+++ src/app/shared/components/abstract-contacts/abstract-contacts.component.ts
@@ -1,60 +1,21 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {ProfessionalContactInline} from '@app/api/models/professional-contact-inline';
-import {HelperService} from '@app/core/services/helper.service';
 import {MediaIconFactoryService} from '@app/core/services/media-icon-factory.service';
-import {Contact} from '@app/profile/models/contact';
-import {ContactApiService} from '@app/profile/services/contact-api.service';
-import {Reinitable} from '@app/shared/abstract/reinitable';
-import {BehaviorSubject} from 'rxjs';
 
 @Component({
     selector: 'app-abstract-contacts',
     templateUrl: './abstract-contacts.component.html',
     styleUrls: ['./abstract-contacts.component.scss']
 })
-export class AbstractContactsComponent extends Reinitable implements OnInit {
+export class AbstractContactsComponent {
 
     @Input() public addNewContactUrl: string = '/profile/contact-add/';
     @Input() public editDefaultContactUrl: string = '/profile/contact-add-default/';
     @Input() public editContactUrl: string = '/profile/contact-edit/';
     @Input() public interactable: boolean = false;
     @Input() public clientContacts: ProfessionalContactInline[];
-    public canAddNewContact$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-    public contacts$: BehaviorSubject<Contact[]> = new BehaviorSubject<Contact[]>([]);
-    public defaultContacts$: BehaviorSubject<Contact[]> = new BehaviorSubject<Contact[]>([]);
-
-    constructor(
-        private readonly contactsApi: ContactApiService
-    ) {
-        super();
-    }
-
-    public ngOnInit(): void {
-        this.init();
-    }
 
     public getContactIcon(contactDisplay: string): string {
         return MediaIconFactoryService.getIcon(contactDisplay);
-    }
-
-    protected init(): void {
-        this.contactsApi.get().subscribe(
-            list => {
-                this.contacts$.next(list.results);
-                this.initDefaultContacts(list.results);
-                this.canAddNewContact(list.results);
-            }
-        );
-    }
-
-    private initDefaultContacts(contacts: Contact[]): void {
-        const defaultContacts: Contact[] = [];
-        contacts.forEach(contact => contact.is_default ? defaultContacts.push(contact) : null);
-
-        this.defaultContacts$.next(defaultContacts);
-    }
-
-    private canAddNewContact(contacts: Contact[]): void {
-        this.canAddNewContact$.next(HelperService.calculateContacts(contacts, this.clientContacts).length === 0);
     }
 }
