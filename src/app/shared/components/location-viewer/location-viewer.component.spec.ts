744aa17a7c9d6c7b7023f40f7b62f649a2fae0b1
enkryptor <enkryptor@yandex.ru>
2020-12-31 00:10:38 +0300

    feat: show full address at service, profile pages

--- src/app/shared/components/location-viewer/location-viewer.component.spec.ts
+++ src/app/shared/components/location-viewer/location-viewer.component.spec.ts
@@ -1,3 +1,4 @@
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 import {IonicModule} from '@ionic/angular';
 
@@ -10,7 +11,7 @@ describe('LocationPresentationComponent', () => {
     beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
             declarations: [LocationViewerComponent],
-            imports: [IonicModule.forRoot()]
+            imports: [IonicModule.forRoot(), HttpClientTestingModule]
         }).compileComponents();
 
         fixture = TestBed.createComponent(LocationViewerComponent);
--- src/app/shared/components/location-viewer/location-viewer.component.ts
+++ src/app/shared/components/location-viewer/location-viewer.component.ts
@@ -1,12 +1,28 @@
-import { Component, Input } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {ProfessionalLocationInline} from '@app/api/models/professional-location-inline';
+import {FullLocationService, LocationInterface} from '@app/core/services/location/full-location.service';
+import {BehaviorSubject, Observable, of} from 'rxjs';
+import {map, switchMap} from 'rxjs/operators';
 
 @Component({
-  selector: 'app-location-viewer',
-  templateUrl: './location-viewer.component.html',
-  styleUrls: ['./location-viewer.component.scss']
+    selector: 'app-location-viewer',
+    templateUrl: './location-viewer.component.html',
+    styleUrls: ['./location-viewer.component.scss']
 })
 export class LocationViewerComponent {
+    public locationText$: Observable<string>;
 
-  @Input() public location: ProfessionalLocationInline;
+    private readonly locationSubject = new BehaviorSubject<ProfessionalLocationInline>(null);
+
+    constructor(private readonly locationService: FullLocationService) {
+        this.locationText$ = this.locationSubject.pipe(
+            switchMap(location => location ? locationService.getTextLocation(location) : of(null)),
+            map(result => result?.text)
+        );
+    }
+
+    @Input()
+    public set location(value: LocationInterface) {
+        this.locationSubject.next(value);
+    }
 }
