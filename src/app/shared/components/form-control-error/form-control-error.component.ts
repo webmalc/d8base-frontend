ec170524209f1e2c7906d33f2fb86d492f086768
Slepukhin Vitalii <vitslepukhin@gmail.com>
2021-01-13 23:49:24 +0400

    fix(validation): Use async pipe

--- src/app/shared/components/form-control-error/form-control-error.component.ts
+++ src/app/shared/components/form-control-error/form-control-error.component.ts
@@ -1,5 +1,7 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit, Optional, SkipSelf } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit, Optional, SkipSelf } from '@angular/core';
 import { AbstractControl, ControlContainer } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-form-control-error',
@@ -15,25 +17,23 @@ export class FormControlErrorComponent implements OnInit {
   @Input() set controlName(name: string) {
     this.control = this.controlContainer?.control.get(name);
   }
-  public errors: { key: string; param: any }[];
+  public errors: Observable<{ key: string; param: any }[]>;
 
-  constructor(@Optional() @SkipSelf() private readonly controlContainer: ControlContainer, private readonly cd: ChangeDetectorRef) {}
+  constructor(@Optional() @SkipSelf() private readonly controlContainer: ControlContainer) {}
 
   public ngOnInit(): void {
-    this.control?.valueChanges.subscribe(() => {
-      if (!this.control.invalid && !this.control.dirty) {
-        return;
-      }
-      this.errors = this.control.errors
-        ? Object.keys(this.control.errors)
-            .filter(key => !this.filterErrors.includes(key))
-            .filter(key => !Boolean(this.showErrors?.length) || this.showErrors.includes(key))
-            .map(key => ({
-              key: this.errorDescriptions?.[key] ?? `form-errors.${key}`,
-              param: this.control.errors[key],
-            }))
-        : [];
-      this.cd.markForCheck();
-    });
+    this.errors = this.control.statusChanges.pipe(
+      map(() =>
+        this.control.dirty && this.control.errors
+          ? Object.keys(this.control.errors)
+              .filter(key => !this.filterErrors.includes(key))
+              .filter(key => !Boolean(this.showErrors?.length) || this.showErrors.includes(key))
+              .map(key => ({
+                key: this.errorDescriptions?.[key] ?? `form-errors.${key}`,
+                param: this.control.errors[key],
+              }))
+          : [],
+      ),
+    );
   }
 }
