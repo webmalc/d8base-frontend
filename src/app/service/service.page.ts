6c7189de41515c5113c726097f160311eff534bc
enkryptor <enkryptor@yandex.ru>
2021-02-16 19:32:34 +0300

    fix: remove unused component

--- src/app/service/service.page.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-service',
-  templateUrl: './service.page.html',
-  styleUrls: ['./service.page.scss'],
-})
-export class ServicePage {
-}
