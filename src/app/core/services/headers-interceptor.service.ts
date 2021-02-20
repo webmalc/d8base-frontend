a6bb323a1239c75b6124fa1c1f8261eced9f2127
enkryptor <enkryptor@yandex.ru>
2021-02-20 16:45:08 +0300

    fix: error 401 on page refresh

--- src/app/core/services/headers-interceptor.service.ts
+++ src/app/core/services/headers-interceptor.service.ts
@@ -5,7 +5,7 @@ import CurrentUserSelectors from '@app/store/current-user/current-user.selectors
 import { environment } from '@env/environment';
 import { Select } from '@ngxs/store';
 import { Observable } from 'rxjs';
-import { first, map, single, switchMap } from 'rxjs/operators';
+import { first, map, switchMap } from 'rxjs/operators';
 
 /**
  * Sets headers while requesting api endpoints
@@ -19,36 +19,32 @@ export class HeadersInterceptor implements HttpInterceptor {
   public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     try {
       const url = new URL(req.url);
-      if (url.origin !== environment.backend.url ) {
+      if (url.origin !== environment.backend.url) {
         return next.handle(req);
       }
     } catch (e) {
       return next.handle(req);
     }
 
+    const isTokensRequest = req.url === environment.backend.url + environment.backend.auth;
+    const isRefreshRequest = req.url === environment.backend.url + environment.backend.refresh;
     return this.tokens$
       .pipe(
-        first(),
+        first(tokens => isTokensRequest || Boolean(tokens)),
         map(tokens => tokens?.access_token),
         switchMap(token => {
           let headers;
-          if (this.getAuthUrls().includes(req.url)) {
+          if (isRefreshRequest) {
             headers = req.headers.append('Authorization', `Basic ${
               btoa(`${environment.client_id}:${environment.client_secret}`)}`)
               .append('Content-Type', 'application/json');
           } else if (token) {
-            headers = req.headers.append('Authorization', `Bearer ${  token}`)
+            headers = req.headers.append('Authorization', `Bearer ${token}`)
               .append('Content-Type', 'application/json');
           }
 
           return next.handle(req.clone({ headers }));
         }),
       );
-  }
-
-  private getAuthUrls(): string[] {
-    return [
-      environment.backend.url + environment.backend.refresh,
-    ];
   }
 }
