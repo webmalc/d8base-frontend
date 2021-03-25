a16757488388bcfd424911a3b3ca3342a41c1b04
Slepukhin Vitalii <vitslepukhin@gmail.com>
2021-03-25 17:17:49 +0400

    feat(shared): Add contry code to phone editor

--- src/app/shared/components/phone-editor/phone-editor.component.ts
+++ src/app/shared/components/phone-editor/phone-editor.component.ts
@@ -1,6 +1,12 @@
-import { Component, forwardRef, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, forwardRef } from '@angular/core';
 import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Country } from '@app/api/models';
+import { LocationService } from '@app/api/services';
+import { NgDestroyService } from '@app/core/services';
+import { map, takeUntil } from 'rxjs/operators';
 
+const PAGE_SIZE = 250;
+const DEFAULT_COUNTRY: Partial<Country> = { id: 6251999, name: 'Canada', tld: 'ca', phone: '+1' };
 @Component({
   selector: 'app-phone-editor',
   templateUrl: './phone-editor.component.html',
@@ -11,13 +17,29 @@ import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR } from '@angular/f
       useExisting: forwardRef(() => PhoneEditorComponent),
       multi: true,
     },
+    NgDestroyService,
   ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PhoneEditorComponent implements ControlValueAccessor {
-  public innerControl = new FormControl();
+  public phoneControl = new FormControl();
+  public countryControl = new FormControl(DEFAULT_COUNTRY);
+  public previousPhoneValue: string = '';
+  public countries$ = this.locationApi.locationCountriesList({ pageSize: PAGE_SIZE }).pipe(map(x => x.results));
+  public title = 'location-edit-page.country';
+  private onChange: (value: string) => void;
+
+  constructor(
+    private readonly locationApi: LocationService,
+    private readonly ngDestroy$: NgDestroyService,
+    private readonly cd: ChangeDetectorRef,
+  ) {
+    this.subscribePhoneCodeControlValueChanges();
+    this.subscribeCountryCodeControlValueChanges();
+  }
 
   public registerOnChange(fn: any): void {
-    this.innerControl.registerOnChange(fn);
+    this.onChange = fn;
   }
 
   public registerOnTouched(fn: any): void {
@@ -26,39 +48,33 @@ export class PhoneEditorComponent implements ControlValueAccessor {
 
   public setDisabledState(isDisabled: boolean): void {
     if (isDisabled) {
-      this.innerControl.disable();
+      this.countryControl.disable();
+      this.phoneControl.disable();
     } else {
-      this.innerControl.enable();
+      this.countryControl.enable();
+      this.phoneControl.enable();
     }
   }
 
   public writeValue(value: string): void {
-    this.innerControl.setValue(value, { emitEvent: false });
+    // do nothing
   }
 
-  public onPhoneFocus(phone: string): void {
-    if (phone === '') {
-      this.innerControl.setValue('+');
-    }
+  public plusSignEnforce(countryPhone: Country['phone']): string {
+    return !countryPhone?.length || countryPhone?.includes('+') ? countryPhone : `+${countryPhone}`;
   }
 
-  public onPhoneBlur(phone: string): void {
-    if (phone === '+') {
-      this.innerControl.setValue('');
-    }
+  private subscribeCountryCodeControlValueChanges(): void {
+    this.countryControl.valueChanges.pipe(takeUntil(this.ngDestroy$)).subscribe(() => {
+      this.cd.markForCheck();
+    });
   }
 
-  public onPhoneInputChange(phone: string): void {
-    const inputNumericVal = phone.replace(/\D/g, '');
-    let inputNewValue = '';
-    inputNewValue += `+${inputNumericVal}`;
-
-    this.innerControl.setValue(inputNewValue);
-  }
-
-  public onPhoneChange(phone: string): void {
-    if (phone !== '+' && isNaN(parseInt(phone, 10)) && phone.slice(-1) === phone) {
-      this.innerControl.setValue(phone.slice(0, -1));
-    }
+  private subscribePhoneCodeControlValueChanges(): void {
+    this.phoneControl.valueChanges.pipe(takeUntil(this.ngDestroy$)).subscribe(phone => {
+      const countryCode = this.plusSignEnforce(this.countryControl.value.phone);
+      this.onChange(`${countryCode}${phone}`);
+      this.cd.markForCheck();
+    });
   }
 }
