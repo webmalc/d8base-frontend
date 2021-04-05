0b8c2d82f6ed34ea7ee2cad8c9d4f0365a578ebd
enkryptor <enkryptor@yandex.ru>
2021-04-05 20:53:51 +0300

    chore: remove theme selector from regional settings

--- src/app/shared/components/flag-menu/flag-menu.component.ts
+++ src/app/shared/components/flag-menu/flag-menu.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Rate, UserLocation } from '@app/api/models';
-import { DarkModeService } from '@app/core/services';
 import { RatesApiCache } from '@app/core/services/cache';
 import { UserManagerService } from '@app/core/services/user-manager.service';
 import { UserSettingsService } from '@app/shared/services/user-settings.service';
@@ -14,22 +13,14 @@ import { filter } from 'rxjs/operators';
 })
 export class FlagMenuComponent {
   public defaultLocation$: Observable<UserLocation>;
-  public darkTheme$: Observable<boolean>;
   public rateList$: Observable<Rate[]>;
 
   constructor(
     public readonly userSettingsService: UserSettingsService,
-    private readonly darkModeService: DarkModeService,
     userManager: UserManagerService,
     rates: RatesApiCache,
   ) {
-    this.darkTheme$ = darkModeService.darkTheme$;
     this.defaultLocation$ = userManager.defaultLocation$.pipe(filter(x => !!x));
     this.rateList$ = rates.list();
-  }
-
-  public changeMode(event: CustomEvent): void {
-    const toggle = event.target as HTMLIonToggleElement;
-    this.darkModeService.setMode(toggle.checked);
   }
 }
