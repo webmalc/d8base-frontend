a66ea0774c6187d317f593a2c740cd7a09a1f5d0
Slepukhin Vitalii <vitslepukhin@gmail.com>
2021-04-28 20:01:11 +0400

    fix(profile): Fix test ChangeEmailComponent

--- src/app/profile/components/change-email/change-email.component.spec.ts
+++ src/app/profile/components/change-email/change-email.component.spec.ts
@@ -1,13 +1,7 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { FormBuilder } from '@angular/forms';
-import { StorageManagerService } from '@app/core/proxies/storage-manager.service';
-import { SelectableCountryOnSearchService } from '@app/shared/services/selectable-country-on-search.service';
 import { Actions } from '@ngxs/store';
 import { ComponentTestingModule, RootModules } from 'src/testing/component-testing.module';
-import { StorageManagerMock } from 'src/testing/mocks';
-import { SavedProfessionalApiService } from '../../services/saved-professional-api.service';
 import { ChangeEmailComponent } from './change-email.component';
-
 
 describe('ChangeEmailComponent', () => {
   let component: ChangeEmailComponent;
@@ -18,13 +12,7 @@ describe('ChangeEmailComponent', () => {
       TestBed.configureTestingModule({
         declarations: [ChangeEmailComponent],
         imports: [...RootModules(), ComponentTestingModule],
-        providers: [
-          Actions,
-          SelectableCountryOnSearchService,
-          FormBuilder,
-          SavedProfessionalApiService,
-          { provide: StorageManagerService, useClass: StorageManagerMock },
-        ],
+        providers: [Actions],
       }).compileComponents();
 
       fixture = TestBed.createComponent(ChangeEmailComponent);
