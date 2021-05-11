9328addd57fafdb17029a53ec44eb51fce787738
Slepukhin Vitalii <vitslepukhin@gmail.com>
2021-05-11 14:13:40 +0400

    fix(reviews): Fix bug when unauthenticated

--- src/app/core/services/user-manager.service.ts
+++ src/app/core/services/user-manager.service.ts
@@ -11,6 +11,8 @@ import { first } from 'rxjs/operators';
   providedIn: 'root',
 })
 export class UserManagerService {
+  @Select(CurrentUserSelectors.isAuthenticated)
+  public isAuthenticated$: Observable<boolean>;
 
   @Select(CurrentUserSelectors.profile)
   public profile$: Observable<Profile>;
