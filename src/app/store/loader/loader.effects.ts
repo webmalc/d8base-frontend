daba38f58223c07936ed68398d721cff9c6807b4
Slepukhin Vitalii <vitslepukhin@gmail.com>
2021-07-07 23:28:47 +0400

    feat(store, loader): Improve loader effects, don't need to use actions array

--- src/app/store/loader/loader.effects.ts
+++ src/app/store/loader/loader.effects.ts
@@ -1,22 +1,41 @@
 import { Injectable } from '@angular/core';
-import { Actions, ofActionCompleted, ofActionDispatched, ofActionErrored, Store } from '@ngxs/store';
-import * as CurrentUserActions from '../current-user/current-user.actions';
-import * as ProfessionalActions from '../professional-page/professional-page.actions';
+import { Actions, Store } from '@ngxs/store';
+import { ActionContext, ActionStatus } from '@ngxs/store/src/actions-stream';
+import { Observable, OperatorFunction } from 'rxjs';
+import { filter } from 'rxjs/operators';
 import * as LoaderActions from './loader.actions';
+import { LoaderAction } from './types/loader-action.type';
 
-const loaderActions = [CurrentUserActions.LoadProfile, ProfessionalActions.LoadProfessionalById];
+const ShowLoaderStatuses: ActionStatus[] = [ActionStatus.Dispatched];
+const HideLoaderStatuses: ActionStatus[] = [ActionStatus.Successful, ActionStatus.Canceled, ActionStatus.Errored];
 
+function ofShowLoaderAction(): OperatorFunction<ActionContext, any> {
+  return function (o: Observable<ActionContext>) {
+    return o.pipe(filterLoaderActionsStatus(ShowLoaderStatuses));
+  };
+}
+
+function ofHideLoaderAction(): OperatorFunction<ActionContext, any> {
+  return function (o: Observable<ActionContext>) {
+    return o.pipe(filterLoaderActionsStatus(HideLoaderStatuses));
+  };
+}
+
+function filterLoaderActionsStatus(allowedStatuses: ActionStatus[]) {
+  return filter((ctx: ActionContext<LoaderAction>) => {
+    const loaderTypeMatch = ctx.action?.loaderKey;
+    const statusMatch = allowedStatuses.includes(ctx.status);
+    return loaderTypeMatch && statusMatch;
+  });
+}
 @Injectable({ providedIn: 'root' })
 export class LoaderEffects {
   constructor(actions$: Actions, store: Store) {
-    actions$.pipe(ofActionDispatched(...loaderActions)).subscribe(action => {
+    actions$.pipe(ofShowLoaderAction()).subscribe(({ action }) => {
       store.dispatch(new LoaderActions.ShowLoader(action.loaderKey));
     });
 
-    actions$.pipe(ofActionCompleted(...loaderActions)).subscribe(({ action, result }) => {
-      if (result.error) {
-        console.error('result.error', result.error);
-      }
+    actions$.pipe(ofHideLoaderAction()).subscribe(({ action }) => {
       store.dispatch(new LoaderActions.HideLoader(action.loaderKey));
     });
   }
