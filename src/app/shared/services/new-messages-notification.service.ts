3a12702f90bde71b50132260fc5332458416e973
enkryptor <enkryptor@yandex.ru>
2021-07-13 16:44:16 +0300

    fix: only registered users should check for a new messages

--- src/app/shared/services/new-messages-notification.service.ts
+++ src/app/shared/services/new-messages-notification.service.ts
@@ -1,19 +1,38 @@
 import { Injectable } from '@angular/core';
 import { CommunicationService } from '@app/api/services/communication.service';
+import { AuthenticationService } from '@app/core/services';
 import { IntervalService } from '@app/shared/services/interval.service';
 import { environment } from '@env/environment';
-import { shareReplay, switchMap } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { catchError, shareReplay, switchMap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 import { ReceivedMessage } from '@app/api/models';
+
+type ResponseData = { count: number; next?: string; previous?: string; results: ReceivedMessage[] };
+
+const emptyResponse = {
+  count: 0,
+  results: [],
+};
 
 @Injectable()
 export class NewMessagesNotificationService {
-  public unreadMessages$: Observable<{ count: number; next?: string; previous?: string; results: ReceivedMessage[] }>;
+  public unreadMessages$: Observable<ResponseData>;
 
-  constructor(private readonly api: CommunicationService, private readonly interval: IntervalService) {
-    this.unreadMessages$ = this.interval.ticks(environment.message.chat_list_update_interval_ms).pipe(
-      switchMap(() => this.api.communicationMessagesReceivedList({ isRead: 'false' })),
+  constructor(
+    private readonly auth: AuthenticationService,
+    private readonly api: CommunicationService,
+    private readonly interval: IntervalService,
+  ) {
+    this.unreadMessages$ = this.auth.isAuthenticated$.pipe(
+      switchMap(isAuthenticated => (isAuthenticated ? this.getNewMessages() : of(emptyResponse))),
+      catchError(() => of(emptyResponse)),
       shareReplay(1),
     );
+  }
+
+  private getNewMessages(): Observable<ResponseData> {
+    return this.interval
+      .ticks(environment.message.chat_list_update_interval_ms)
+      .pipe(switchMap(() => this.api.communicationMessagesReceivedList({ isRead: 'false' })));
   }
 }
