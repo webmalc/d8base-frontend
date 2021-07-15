a0b1c26b599020edc5785248404ab7d110e51ccf
ennaque <volkov.enn@gmail.com>
2021-07-15 04:01:19 +0300

    move contact logos to env

--- src/app/core/services/media-icon-factory.service.ts
+++ src/app/core/services/media-icon-factory.service.ts
@@ -1,27 +1,13 @@
 import { Injectable } from '@angular/core';
+import {environment} from '@env/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MediaIconFactoryService {
   public static getIcon(media: string): string {
-    switch (media) {
-      case 'facebook':
-        return 'logo-facebook';
-      case 'instagram':
-        return 'logo-instagram';
-      case 'web':
-        return 'globe-outline';
-      case 'skype':
-        return 'logo-skype';
-      case 'phone':
-        return 'call-outline';
-      case 'whatsapp':
-        return 'logo-whatsapp';
-      case 'E-mail':
-        return 'mail-outline';
-      default:
-        return 'reader-outline';
-    }
+    const contacts: object = environment.contacts;
+
+    return contacts[media] ?? contacts['default'];
   }
 }
