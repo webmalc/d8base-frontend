d39b2b72bf058994cda4a8a87d3ee9ea5dfaee63
ennaque <volkov.enn@gmail.com>
2021-07-15 14:03:07 +0300

    hardcode default logo

--- src/app/core/services/media-icon-factory.service.ts
+++ src/app/core/services/media-icon-factory.service.ts
@@ -7,7 +7,8 @@ import { environment } from '@env/environment';
 export class MediaIconFactoryService {
   public static getIcon(media: string): string {
     const contacts = environment.contacts;
+    const defaultLogo = 'reader-outline'
 
-    return contacts[media] ?? contacts.default;
+    return contacts[media] ?? defaultLogo;
   }
 }
