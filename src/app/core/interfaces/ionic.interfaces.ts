f109789723cb24e7401efcce616c18711e399948
enkryptor <enkryptor@yandex.ru>
2021-08-24 18:41:24 +0300

    chore: prepare the new service wizard

--- src/app/core/interfaces/ionic.interfaces.ts
+++ src/app/core/interfaces/ionic.interfaces.ts
@@ -1,3 +1,11 @@
+export interface IonViewWillEnter {
+  ionViewWillEnter(): void;
+}
+
 export interface IonViewDidEnter {
   ionViewDidEnter(): void;
+}
+
+export interface IonViewDidLeave {
+  ionViewDidLeave(): void;
 }
