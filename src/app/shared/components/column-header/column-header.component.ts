8105cf970ef6f34c02669975130e9cd858147e82
enkryptor <enkryptor@yandex.ru>
2021-08-31 18:17:05 +0300

    refactor: using NavController.back() instead of custom solution

--- src/app/shared/components/column-header/column-header.component.ts
+++ src/app/shared/components/column-header/column-header.component.ts
@@ -1,8 +1,6 @@
-import { Component, Input } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { parseUriString } from '@app/core/functions/uri.functions';
-import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
-import { first, map } from 'rxjs/operators';
+import { Component, Inject, Input } from '@angular/core';
+import { WINDOW } from '@app/core/injection-tokens';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-column-header',
@@ -12,33 +10,20 @@ export class ColumnHeaderComponent {
   @Input()
   public transparent: boolean = false;
 
-  public backButtonLink$: Observable<string>;
-  public backButtonParams$: Observable<object>;
-
-  private readonly backButtonUrl$ = new BehaviorSubject<string>('');
-  private readonly url$: Observable<{ path: string; queryParams?: object }>;
-
-  constructor(private readonly activatedRoute: ActivatedRoute, private readonly router: Router) {
-    if (!this.activatedRoute?.queryParamMap) {
-      return;
-    }
-    this.url$ = combineLatest([this.activatedRoute.queryParamMap, this.backButtonUrl$]).pipe(
-      map(([paramsMap, backButtonUrl]) => {
-        const param = paramsMap.get('redirectTo');
-        const redirectTo = param ? decodeURIComponent(param) : backButtonUrl;
-        return parseUriString(redirectTo);
-      }),
-    );
-    this.backButtonLink$ = this.url$.pipe(map(url => url.path));
-    this.backButtonParams$ = this.url$.pipe(map(url => url.queryParams));
-  }
-
   @Input()
-  public set backButtonUrl(url: string) {
-    this.backButtonUrl$.next(url);
+  public backButtonUrl: string;
+
+  constructor(private readonly navController: NavController, @Inject(WINDOW) private readonly window: Window) {}
+
+  public navigateBack() {
+    if (this.canGoBack()) {
+      this.navController.back();
+    } else if (this.backButtonUrl) {
+      this.navController.navigateBack(this.backButtonUrl);
+    }
   }
 
-  public navigateBack(): void {
-    this.url$.pipe(first()).subscribe(url => this.router.navigate([url.path], { queryParams: url.queryParams }));
+  private canGoBack(): boolean {
+    return this.window.history.length > 1;
   }
 }
