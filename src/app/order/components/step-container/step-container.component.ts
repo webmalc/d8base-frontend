4f8a671d01f840a0121b656d24e5fccb16b92871
enkryptor <enkryptor@yandex.ru>
2021-10-14 18:43:12 +0300

    feat(order): don't reset the order state on leave

--- src/app/order/components/step-container/step-container.component.ts
+++ src/app/order/components/step-container/step-container.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProfessionalList } from '@app/api/models/professional-list';
 import { isFormInvalid } from '@app/core/functions/form.functions';
+import { getProfessionalServicesUrl } from '@app/core/functions/navigation.functions';
 import { NgDestroyService } from '@app/core/services';
 import { StepComponent } from '@app/order/abstract/step';
 import { OrderIds } from '@app/order/enums/order-ids.enum';
@@ -8,7 +10,7 @@ import StepContext from '@app/order/interfaces/step-context.interface';
 import StepModel from '@app/order/interfaces/step-model.interface';
 import { OrderWizardStateService } from '@app/order/services';
 import { Observable } from 'rxjs';
-import { take, takeUntil } from 'rxjs/operators';
+import { map, take, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-step-container',
@@ -26,10 +28,15 @@ export class StepContainerComponent implements OnInit {
   constructor(
     private readonly wizardState: OrderWizardStateService,
     private readonly route: ActivatedRoute,
+    private readonly router: Router,
     private readonly cd: ChangeDetectorRef,
     private readonly stepComponent: StepComponent<unknown>,
     private readonly ngDestroy$: NgDestroyService,
   ) {}
+
+  public get professional$(): Observable<ProfessionalList> {
+    return this.context$.pipe(map(x => x.professional));
+  }
 
   public ngOnInit(): void {
     this.subscribeInputContext();
@@ -58,6 +65,14 @@ export class StepContainerComponent implements OnInit {
 
     this.wizardState.setCurrentStepState(this.stepComponent.outputData);
     this.wizardState.doSubmit();
+  }
+
+  public async reset(professionalId: number): Promise<void> {
+    if (!professionalId) {
+      return;
+    }
+    this.wizardState.resetWizard();
+    await this.router.navigateByUrl(getProfessionalServicesUrl(professionalId));
   }
 
   private subscribeInputCurrentState(): void {
